c93579c0f9554409b32e19daa78f45030df95dd1
Arthur <arthur.gamblin@epitech.eu>
2019-02-13 01:56:17 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					<p>It's fucking working</p>
+					<p>heyho</p>
 					Yaaaa
 				</header>
 			</div>
